13e2dd483e11e74a5344112e456280fac91ae121
Guilherme Hermes <85593021+GuilhermeHermes@users.noreply.github.com>
2024-09-17 14:25:41 -0300

    referenciando posts do usuario

--- src/main/java/com/guilhermehermes/domain/User.java
+++ src/main/java/com/guilhermehermes/domain/User.java
@@ -1,6 +1,7 @@
 package com.guilhermehermes.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.lang.NonNull;
 
@@ -9,6 +10,8 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern.Flag;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -26,6 +29,8 @@ public class User implements Serializable {
 
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User(String id, String name, String email) {
         this.id = id;
@@ -59,6 +64,14 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
